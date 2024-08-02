9e25c28581828b7bad4f2a4042a3b8786086081b
Elodie <elodie.benard76@gmail.com>
2024-08-02 18:23:11 +0200

    correction de l'affichage des mois dans les events cards et du filtrage des events

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -16,10 +16,11 @@ const Select = ({
   const [value, setValue] = useState();
   const [collapsed, setCollapsed] = useState(true);
   const changeValue = (newValue) => {
-    onChange();
+    onChange(newValue);
     setValue(newValue);
-    setCollapsed(newValue);
+    setCollapsed(true);
   };
+  console.log(selection);
   return (
     <div className={`SelectContainer ${type}`} data-testid="select-testid">
       {label && <div className="label">{label}</div>}
